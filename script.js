07520b096af595a6dcf891a471bdf763ba2379a6
connorwarme <connor.warme@gmail.com>
2021-10-12 14:27:24 -0600

    picking up where I left off

--- script.js
+++ script.js
@@ -1,26 +1,21 @@
-console.log("Hello, World")
-
 // function called computerPlay that will randomly return: 'Rock', 'Paper', or 'Scissors'.
-//
 
-function computerPlay () {
+let computerChoice = function computerPlay() {
     let options = ['rock', 'paper', 'scissors']; 
     let computerAnswer = options[Math.floor(Math.random()*options.length)];
     console.log(computerAnswer);
     return (computerAnswer);
  }
 
-function playerPlay () {
+let playerChoice = function playerPlay() {
    return (prompt('Input your selection: rock, paper, or scissors!').toLowerCase());
 }
 // want to run a check to confirm proper player input
 
-play (playerPlay(), computerPlay())
-
-function play(playerSelection, computerSelection) {
+let rps = function play(playerSelection, computerSelection) {
     if (playerSelection == computerSelection) {
       alert('You\'ve chosen the same object. Try again!');
-      play ();
+      rps ();
   }
     else if ((playerSelection == 'rock' && computerSelection == 'paper') || (playerSelection == 'paper' && computerSelection == 'scissors') || (playerSelection == 'scissors' && computerSelection == 'rock')) {
        alert('You lost!');
@@ -33,12 +28,7 @@ function play(playerSelection, computerSelection) {
    }
 }
 
-//    let input = prompt('Input your selection: rock, paper, or scissors!');
-//console.log(input);
-//function capitalize(input) {
-//return (input.charAt(0).toUpperCase() + input.slice(1).toLowerCase());
-//};
-//console.log(capitalize());
+
 //if (capitalize(input)!== 'Rock' || 'Paper' || 'Scissors') {
 //    alert('You must choose rock, paper, or scissors! Try again.');
 //    return playerPlay();
@@ -51,10 +41,6 @@ function play(playerSelection, computerSelection) {
 
 
 
-
-
-//const playerSelection = playerPlay();
-//const computerSelection = computerPlay();
 // function that plays one round of rps. 
 // function should take two parameters, playerSelection (case insensitive) and computer Selection
 // then return a string that declares the winner of the round
